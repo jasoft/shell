8ce38eca6367eafa8bb848b4c1b822143bb65f09
John Anders <ja.invent@gmail.com>
2024-02-25 21:06:59 -0600

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,10 @@
+apt-get update
+apt-get install -y dnsutils iputils-ping wget vim git fish python3
+curl -L https://get.oh-my.fish | fish
+fish -c "fish ./installfish.sh --noninteractive"
+fish -c "omf install bobthefish"
+fish -c "omf install z"
+fish -c "omf install fzf"
+fish -c "omf install pj"
+curl https://hishtory.dev/install.py | python3 -
+chsh -s /usr/bin/fish
