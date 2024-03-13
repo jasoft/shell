6eff2381b3f91937e5191b1e4f411c9793b6fc09
John Anders <ja.invent@gmail.com>
2024-03-13 11:04:45 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -11,7 +11,7 @@ chsh -s /usr/bin/fish
 dpkg-reconfigure -f noninteractive tzdata
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen 
 sed -i -e 's/# zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/' /etc/locale.gen 
-echo 'LANG="zh_CN.UTF-8"'>/etc/default/locale
+echo 'LANG="en_US.UTF-8"'>/etc/default/locale
 dpkg-reconfigure --frontend=noninteractive locales
 update-locale LANG=en_US.UTF-8
 
